b926b292601e7e80ccaeb97087a65c6551aca717
Santino Ventrice <santino.ventrice@gmail.com>
2026-06-10 14:26:39 -0300

    Creando clase Partida con Batallas, Pesos e Inventario
    Y moviendo los métodos de manejo de pesos de Equipo a Partida

--- src/modelo/Equipo.java
+++ src/modelo/Equipo.java
@@ -5,12 +5,10 @@ import java.util.List;
 
 public class Equipo {
 	private List<Entidad> entidades;
-	private int pesos;
 	private final int cantidadMaxima = 3;
 	
 	public Equipo() {
 		entidades = new ArrayList<Entidad>();
-		pesos = 0;
 	}
 
 	public List<Entidad> getEntidades() {
@@ -25,22 +23,5 @@ public class Equipo {
 	
 	public void quitarEntidad(Entidad entidad) {
 		entidades.remove(entidad);
-	}
-
-	public int getPesos() {
-		return pesos;
-	}
-	
-	public void recibirPesos(int cantidad) {
-		this.pesos += cantidad;
-	}
-	
-	public boolean quitarPesos(int cantidad) {
-		if (cantidad > 0 && this.pesos >= cantidad) {
-			this.pesos -= cantidad;
-			return true; // Compra exitosa
-		}
-		
-		return false; // No hay suficientes pesos
 	}
 }
---
+++ src/modelo/Partida.java
@@ -0,0 +1,69 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import enums.ESTADO_BATALLA;
+
+public class Partida {
+	private int pesos;
+	private List<Item> inventario;
+	private List<Batalla> batallas;
+	
+	public Partida() {
+		this.pesos = 0;
+		this.inventario = new ArrayList<Item>();
+		this.batallas = new ArrayList<Batalla>();
+		cargarBatallas();
+	}
+
+	public int getPesos() {
+		return pesos;
+	}
+	
+	public void recibirPesos(int cantidad) {
+		this.pesos += cantidad;
+	}
+	
+	public boolean quitarPesos(int cantidad) {
+		if (cantidad > 0 && this.pesos >= cantidad) {
+			this.pesos -= cantidad;
+			return true; // Compra exitosa
+		}
+		
+		return false; // No hay suficientes pesos
+	}
+	
+	public List<Item> getInventario() {
+		return inventario;
+	}
+	
+	public void agregarItem(Item item) {
+		this.inventario.add(item);
+	}
+	
+	public void quitarItem(Item item) {
+		this.inventario.remove(item);
+	}
+
+		public void cargarBatallas() {
+		batallas.add(BatallaFactory.crearNivel1());
+		batallas.add(BatallaFactory.crearNivel2());
+		batallas.add(BatallaFactory.crearNivel3());
+		batallas.add(BatallaFactory.crearNivel4());
+	}
+	
+	public List<Batalla> getBatallas() {
+		return batallas;
+	}
+	
+	public Batalla getProximaBatalla() {
+		for (Batalla b : batallas) {
+			if (b.getEstado() == ESTADO_BATALLA.PENDIENTE) {
+				return b;
+			}
+		}
+		
+		return null;
+	}
+}
