a25355180bfb602401ae0271eb4554d6ea11faf8
Santino Ventrice <santino.ventrice@gmail.com>
2026-06-10 16:54:14 -0300

    Actualizando panel de Equipo y de Jugadores
    Agregando Animaciones idle en los paneles

--- src/views/Jugador3.java
+++ src/views/Jugador3.java
@@ -9,60 +9,46 @@ import javax.swing.JPanel;
 import main.VentanaLayout;
 
 public class Jugador3 extends JPanel {
-	
-	private static final long serialVersionUID = 1L;
-	private Image imagenFondo;
-	
 
+    private static final long serialVersionUID = 1L;
+    private Image imagenFondo;
+    private EntidadView entidadView;
 
     public Jugador3(VentanaLayout ventana) {
-    	
-    	try {
-			imagenFondo= new ImageIcon("src/resources/Inventario-batalla.jpg.jpeg"
-					+ ""
-					+ ""
-					+ "").getImage();
-			
-			
-		} catch (Exception e) {
-			System.out.println("Error al cargar la imagen");
-		}
-		
-    	setLayout(null);
-    	
-    	//imagen de boton
-    	ImageIcon iconoOriginal = new ImageIcon("src/resources/Volver-atras.png");
-    	
-    	Image imageEscalada = iconoOriginal.getImage().getScaledInstance(90, 70, Image.SCALE_SMOOTH);
-    	
-    	ImageIcon iconoExit = new ImageIcon(imageEscalada);
-    	
-    	JButton botonExit = new JButton(iconoExit);
-    	
-    	//posición y tamaño del boton
-    	
-    	botonExit.setBounds(700, 15, 90, 70);
-    	
-    	//Sacar borde y fondo
-    	botonExit.setBorderPainted(false);
-    	botonExit.setContentAreaFilled(false);
-    	botonExit.setFocusPainted(false);
-    	
-    	//Accion al hacer click
-    	botonExit.addActionListener(e -> ventana.volverJugadores());
-    	
-    	add(botonExit);
+
+        try {
+            imagenFondo = new ImageIcon("src/resources/Inventario-batalla.jpg.jpeg").getImage();
+        } catch (Exception e) {
+            System.out.println("Error al cargar la imagen de fondo");
+        }
+
+        setLayout(null);
+
+        // --- ENTIDAD VIEW ---
+        entidadView = new EntidadView("Curandera", 100, 100, 100, 100, false, 2f);
+        entidadView.setBounds(60, 80, 400, 500);
+        add(entidadView);
+
+        // --- BOTÓN VOLVER ---
+        ImageIcon iconoOriginal = new ImageIcon("src/resources/Volver-atras.png");
+        Image imageEscalada = iconoOriginal.getImage().getScaledInstance(90, 70, Image.SCALE_SMOOTH);
+        ImageIcon iconoExit = new ImageIcon(imageEscalada);
+
+        JButton botonExit = new JButton(iconoExit);
+        botonExit.setBounds(700, 15, 90, 70);
+        botonExit.setBorderPainted(false);
+        botonExit.setContentAreaFilled(false);
+        botonExit.setFocusPainted(false);
+        botonExit.addActionListener(e -> ventana.volverJugadores());
+
+        add(botonExit);
     }
-    
-    
+
     @Override
     protected void paintComponent(Graphics g) {
-	   super.paintComponent(g);
-
-        
+        super.paintComponent(g);
         if (imagenFondo != null) {
             g.drawImage(imagenFondo, 0, 0, getWidth(), getHeight(), this);
         }
     }
 }
-
