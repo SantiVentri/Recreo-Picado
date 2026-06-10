311104d008723e798b9fc86343a58abd24c6e536
Santino Ventrice <santino.ventrice@gmail.com>
2026-06-10 02:51:58 -0300

    Creando EfectoFactory para crear los efectos de habilidades

---
+++ src/modelo/EfectoFactory.java
@@ -0,0 +1,21 @@
+package modelo;
+
+import enums.EFECTOS;
+
+public class EfectoFactory {
+    public static Efecto crearVeneno() {
+        return new Efecto("Veneno", "Envenena al objetivo, quitándole vida durante 3 turnos", EFECTOS.VENENO, 3, 8);
+    }
+    
+    public static Efecto crearCuracion() {
+        return new Efecto("Curación", "Regenera vida al objetivo al 85%", EFECTOS.CURACION, 1, 85);
+    }
+    
+    public static Efecto crearRegenerarEnergia() {
+        return new Efecto("Regenerar Energía", "Recupera energía al objetivo al 100%", EFECTOS.REGENERAR_ENERGIA, 1, 100);
+    }
+    
+    public static Efecto crearAtaqueMultiple() {
+        return new Efecto("Ataque Múltiple", "El ataque impacta a todos los enemigos simultáneamente", EFECTOS.ATAQUE_MULTIPLE, 1, 0);
+    }
+}
