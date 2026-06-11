55f3bf689304b4d3affe886c4d6dc91571cd04a8
Santino Ventrice <santino.ventrice@gmail.com>
2026-06-10 23:36:15 -0300

    Agregando carga de personajes en Equipo

--- src/modelo/Partida.java
+++ src/modelo/Partida.java
@@ -7,12 +7,17 @@ import enums.ESTADO_BATALLA;
 
 public class Partida {
 	private int pesos;
+	private Equipo alumnos;
 	private List<Item> inventario;
 	private List<Batalla> batallas;
 	
 	public Partida() {
 		this.pesos = 0;
 		this.inventario = new ArrayList<Item>();
+		
+		this.alumnos = new Equipo();
+		cargarEquipo();
+		
 		this.batallas = new ArrayList<Batalla>();
 		cargarBatallas();
 	}
@@ -44,6 +49,17 @@ public class Partida {
 	
 	public void quitarItem(Item item) {
 		this.inventario.remove(item);
+	}
+	
+	public Equipo getAlumnos() {
+		return alumnos;
+	}
+	
+	public void cargarEquipo() {
+		alumnos.agregarEntidad(PersonajeFactory.crearMago());
+		alumnos.agregarEntidad(PersonajeFactory.crearArquero());
+		alumnos.agregarEntidad(PersonajeFactory.crearCurandera());
+		alumnos.agregarEntidad(PersonajeFactory.crearGuerrero());
 	}
 
 	public void cargarBatallas() {
