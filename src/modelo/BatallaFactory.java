304565d3b147ce30309f28ec4dde3cc45671c6ea
Santino Ventrice <santino.ventrice@gmail.com>
2026-06-10 23:01:30 -0300

    Comentando temporalmente métodos sin implementar

--- src/modelo/BatallaFactory.java
+++ src/modelo/BatallaFactory.java
@@ -1,36 +1,40 @@
 package modelo;
 
+import enums.ESTADO_BATALLA;
+
 public class BatallaFactory {
 	public static Batalla crearNivel1() {
 		Equipo enemigos = new Equipo();
 		enemigos.agregarEntidad(PersonajeFactory.crearJefe1());
 		
-		return new Batalla(enemigos, "baño");
+		Batalla nivel1 = new Batalla(enemigos, "baño");
+		nivel1.setEstado(ESTADO_BATALLA.PENDIENTE);
+		return nivel1;
 	}
 	
 	public static Batalla crearNivel2() {
 		Equipo enemigos = new Equipo();
-		enemigos.agregarEntidad(PersonajeFactory.crearJefe2());
-		enemigos.agregarEntidad(PersonajeFactory.crearSecuaz2());
+		// enemigos.agregarEntidad(PersonajeFactory.crearJefe2());
+		// enemigos.agregarEntidad(PersonajeFactory.crearSecuaz2());
 		
 		return new Batalla(enemigos, "enfermeria");
 	}
 	
 	public static Batalla crearNivel3() {
 		Equipo enemigos = new Equipo();
-		enemigos.agregarEntidad(PersonajeFactory.crearJefe3());
-		enemigos.agregarEntidad(PersonajeFactory.crearSecuaz3());
-		enemigos.agregarEntidad(PersonajeFactory.crearSecuaz3());
+		// enemigos.agregarEntidad(PersonajeFactory.crearJefe3());
+		// enemigos.agregarEntidad(PersonajeFactory.crearSecuaz3());
+		// enemigos.agregarEntidad(PersonajeFactory.crearSecuaz3());
 		
 		return new Batalla(enemigos, "patio");
 	}
 	
 	public static Batalla crearNivel4() {
 		Equipo enemigos = new Equipo();
-		enemigos.agregarEntidad(PersonajeFactory.crearJefeFinal());
-		enemigos.agregarEntidad(PersonajeFactory.crearSecuaz4());
-		enemigos.agregarEntidad(PersonajeFactory.crearSecuaz4());
-		enemigos.agregarEntidad(PersonajeFactory.crearSecuaz4());
+		// enemigos.agregarEntidad(PersonajeFactory.crearJefeFinal());
+		// enemigos.agregarEntidad(PersonajeFactory.crearSecuaz4());
+		// enemigos.agregarEntidad(PersonajeFactory.crearSecuaz4());
+		// enemigos.agregarEntidad(PersonajeFactory.crearSecuaz4());
 		
 		return new Batalla(enemigos, "oficina");
 	}
