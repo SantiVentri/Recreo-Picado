420ea12c7b602bcdd1fbb2098cbcf87b91957c78
Santino Ventrice <110856430+SantiVentri@users.noreply.github.com>
2026-06-22 18:21:57 -0300

    feat: Extraer clase Alumno con lógica de XP
    Se extraen los métodos repetitivos que usan los alumnos en una clase abstracta Alumno. Se pone en medio de Entidad y las clases como Mago, Guerrero, Arquero y Curandera.

--- src/modelo/Arquero.java
+++ src/modelo/Arquero.java
@@ -2,19 +2,12 @@ package modelo;
 
 import interfaces.IEntidad;
 
-public class Arquero extends Entidad {
-	// Atributos de nivel
-	private int nivel;
-	private int xp;
-
+public class Arquero extends Alumno {
     private int punteria;
 
-    public Arquero(String nombre, int vidaMax, int energiaMax, int velocidad,
-                   int ataque, int defensa, Habilidad habilidad, int punteria) {
+    public Arquero(String nombre, int vidaMax, int energiaMax, int velocidad, int ataque, int defensa, Habilidad habilidad, int punteria) {
         super(nombre, vidaMax, energiaMax, velocidad, ataque, defensa, habilidad);
         this.punteria = punteria;
-		this.nivel = 1;
-		this.xp = 0;
     }
 
     @Override
@@ -51,36 +44,14 @@ public class Arquero extends Entidad {
         }
     }
     
-	private void subirNivel() {
-	    this.nivel++;
-	    this.setAtaque(getAtaque() + 3);
-	    this.setDefensa(getDefensa() + 2);;
-	    this.setVidaMax(getVidaMax() + 10);;
-	}
+    // Getters y setters
+    public int getPunteria() {
+    	return punteria;
+    }
     
-	public void recibirXp(int xp) {
-		this.xp += xp;
-		
-		while (this.xp >= 100) {
-			this.xp -= 100;
-			subirNivel(); 
-		}
-	}
-	
-	public int getNivel() {
-		return nivel;
-	}
-
-	public void setNivel(int nivel) {
-		this.nivel = nivel;
-	}
-
-	public int getXp() {
-		return xp;
-	}
-
-    public int getPunteria() { return punteria; }
-    public void setPunteria(int punteria) { this.punteria = punteria; }
+    public void setPunteria(int punteria) {
+    	this.punteria = punteria;
+    }
     
    
 }
--- src/modelo/Curandera.java
+++ src/modelo/Curandera.java
@@ -6,19 +6,12 @@ import java.util.List;
 import interfaces.IEntidad;
 import orquestador.Orquestador;
  
-public class Curandera extends Entidad {
-	// Atributos de nivel
-	private int nivel;
-	private int xp;
- 
+public class Curandera extends Alumno { 
     private int amistad;
  
-    public Curandera(String nombre, int vidaMax, int energiaMax, int velocidad,
-                     int ataque, int defensa, Habilidad habilidad, int amistad) {
+    public Curandera(String nombre, int vidaMax, int energiaMax, int velocidad, int ataque, int defensa, Habilidad habilidad, int amistad) {
         super(nombre, vidaMax, energiaMax, velocidad, ataque, defensa, habilidad);
         this.amistad = amistad;
-		this.nivel = 1;
-		this.xp = 0;
     }
  
     @Override
@@ -67,35 +60,13 @@ public class Curandera extends Entidad {
         int curacion = hab.getPotencia() + amistad;
         objetivo.aumentarVida(curacion);
     }
-    
-	private void subirNivel() {
-	    this.nivel++;
-	    this.setAtaque(getAtaque() + 3);
-	    this.setDefensa(getDefensa() + 2);;
-	    this.setVidaMax(getVidaMax() + 10);;
-	}
-    
-	public void recibirXp(int xp) {
-		this.xp += xp;
-		
-		while (this.xp >= 100) {
-			this.xp -= 100;
-			subirNivel(); 
-		}
-	}
-	
-	public int getNivel() {
-		return nivel;
-	}
-
-	public void setNivel(int nivel) {
-		this.nivel = nivel;
-	}
-
-	public int getXp() {
-		return xp;
-	}
  
-    public int getAmistad() { return amistad; }
-    public void setAmistad(int amistad) { this.amistad = amistad; }   
+    // Getters y setters
+    public int getAmistad() {
+    	return amistad;
+    }
+    
+    public void setAmistad(int amistad) {
+    	this.amistad = amistad;
+    }   
 }
